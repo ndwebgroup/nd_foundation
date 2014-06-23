1bbba3dcdb308392187714ba314956e1aa615b09
Alec Hipshear <alechipshear@gmail.com>
2014-06-23 09:28:39 -0400

    make the whole thing work as a plugin

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require foundation
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
